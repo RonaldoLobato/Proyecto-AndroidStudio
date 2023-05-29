2ec446892547c2f89577e52028b3c1e8f4a055e2
fullstackprog <ronaldLobato27@outlook.com>
2023-05-28 22:43:55 -0500

    Iniciando el proyecto Inti-Electrodomesticos

---
+++ app/src/main/java/com/example/proyecto_inti_electrodomesticos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_inti_electrodomesticos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
