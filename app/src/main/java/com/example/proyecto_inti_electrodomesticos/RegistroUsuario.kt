d6e324f515968b7b190dc1be6c5d4949748370f8
jhon <ch9ino@gmail.com>
2023-05-29 19:46:13 -0500

    validacionesRegistro3

--- app/src/main/java/com/example/proyecto_inti_electrodomesticos/RegistroUsuario.kt
+++ app/src/main/java/com/example/proyecto_inti_electrodomesticos/RegistroUsuario.kt
@@ -1,11 +1,31 @@
 package com.example.proyecto_inti_electrodomesticos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class RegistroUsuario : AppCompatActivity() {
+
+     lateinit var btnCancelar : Button
+     lateinit var btnRegistrar: Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
+
+        btnCancelar =findViewById(R.id.btnCancelar2)
+
+
+        btnCancelar.setOnClickListener {
+            val Intent = Intent(this,MainActivity::class.java)
+            startActivity(Intent)
+        }
+
+
     }
 }
