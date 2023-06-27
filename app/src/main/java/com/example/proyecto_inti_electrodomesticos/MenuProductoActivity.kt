7538091d27c0e6b27dce5ffcbf38fd1995693737
jhon <ch9ino@gmail.com>
2023-06-27 01:46:48 -0500

    DelRioMantenimientoProducto

---
+++ app/src/main/java/com/example/proyecto_inti_electrodomesticos/MenuProductoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_inti_electrodomesticos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuProductoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_producto)
+    }
+}
